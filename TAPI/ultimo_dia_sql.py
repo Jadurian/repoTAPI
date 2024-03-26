ac07ca3e658f7a989b93917f70e32f3f5ca1a41b
JonathanAdurian <JonathanAdurian@gmail.com>
2024-03-26 12:10:44 -0300

    Carga diaria automatizada

---
+++ TAPI/ultimo_dia_sql.py
@@ -0,0 +1,40 @@
+#codigo para capturar el último día cargado en la BBDD
+
+import os
+import pandas as pd
+import zipfile
+import pyodbc
+import requests
+import io
+from datetime import datetime, timedelta
+from sqlalchemy import engine
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+server = 'DARCCVWSQL19'
+#server = 'DESKTOP-37ESKFT\SQLEXPRESS'
+database = 'TAPI'
+tabla = 'DiarioTest'
+
+#SELEC * FROM Diario;
+
+connection_string = f'DRIVER=ODBC Driver 17 for SQL Server;SERVER={server};DATABASE={database};Trusted_Connection=yes;'
+
+# Conectar a la base de datos
+connection = pyodbc.connect(connection_string)
+
+# Crear un cursor para ejecutar consultas
+cursor = connection.cursor()
+
+# TODO: Crear acá la tabla en la BBDD
+#  Consulta SQL
+query = "SELECT TOP 1 FECHA FROM DiarioTest ORDER BY FECHA DESC"
+
+# Ejecutar la consulta
+df = pd.read_sql(query, connection)
+
+# Cerrar el cursor
+cursor.close()
+
+ultimo_dia = df['FECHA'][0].isoformat()
