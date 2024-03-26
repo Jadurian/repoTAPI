c261332c1069545057e1341c972e1247dccd941a
JonathanAdurian <JonathanAdurian@gmail.com>
2024-03-26 15:50:36 -0300

    Cargando fecha diaria

--- TAPI/ultimodocumento.py
+++ TAPI/ultimodocumento.py
@@ -1,7 +1,13 @@
 import requests
 
 #Con este script se puede obtener la fecha del último documento cargado por CAMMESA
+def  ultimo_dia_CAMM():
 
-ULTIMAFECHA = requests.get("https://api.cammesa.com/pub-svc/public/obtieneFechaUltimoDocumento?nemo=PARTE_POST_OPERATIVO_UNIF")
-cadena = str(ULTIMAFECHA)
-print(str(ULTIMAFECHA.text[10:-2]))
+    ultimafecha = requests.get("https://api.cammesa.com/pub-svc/public/obtieneFechaUltimoDocumento?nemo=PARTE_POST_OPERATIVO")
+
+    #fecha = str(ultimafecha.text[10:-2])
+    fecha = ultimafecha.text[10:-2]
+
+    
+    
+    return fecha
