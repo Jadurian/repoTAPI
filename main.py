9c214602fba2fc489e0434c5dd925a77e8e89d06
JonathanAdurian <JonathanAdurian@gmail.com>
2024-04-18 16:05:11 -0300

    Primer Commit

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import urllib
 import TAPI
 
 def iterar_entre_fechas(fecha_desde, fecha_hasta):
-    fecha_actual = datetime.strptime(fecha_desde, "%Y-%m-%dT%H:%M:%S")
-    fecha_fin = datetime.strptime(fecha_hasta, "%Y-%m-%dT%H:%M:%S")
+    fecha_actual = datetime.strptime(fecha_desde, "%Y-%m-%dT%H:%M:%S.%f%z")
+    fecha_fin = datetime.strptime(fecha_hasta, "%Y-%m-%dT%H:%M:%S.%f%z")
 
     fecha_actual = fecha_actual.replace(hour=0, minute=0, second=0, microsecond=0)
 
@@ -19,7 +19,6 @@ def iterar_entre_fechas(fecha_desde, fecha_hasta):
         fecha_siguiente = fecha_actual + timedelta(hours=23, minutes=59)
         yield fecha_actual, fecha_siguiente
         fecha_actual += timedelta(days=1)
-
 
 server = 'DARCCVWSQL19'
 database = 'TAPI'
