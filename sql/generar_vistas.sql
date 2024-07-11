4538be03a49ec885fa517cc0d0329c72ae416e7d
JonathanAdurian <JonathanAdurian@gmail.com>
2024-07-11 13:34:59 -0300

    generador de vistas added

---
+++ sql/generar_vistas.sql
@@ -0,0 +1,8 @@
+CREATE VIEW [View_Test3_VG] AS
+SELECT *
+FROM Hist_Bk
+WHERE FECHA < DATEADD(month, -1, DATEADD(month, DATEDIFF(month, 0, GETDATE()), 0))
+UNION ALL
+SELECT *
+FROM Valores_Gen_Diario_Automatico
+WHERE FECHA >= DATEADD(month, -1, DATEADD(month, DATEDIFF(month, 0, GETDATE()), 0));
